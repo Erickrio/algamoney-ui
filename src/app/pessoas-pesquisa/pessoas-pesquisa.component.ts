940c4329e93600478237021a89f258f2e3826b87
Erick Costa Monteiro <erick.monteiro@unigranrio.br>
2021-08-26 11:46:22 -0300

    desafio - criando componentes de  pessoas

--- src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -9,7 +9,22 @@ export class PessoasPesquisaComponent  {
 
   pessoas = [
     { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG',
-      status: 'Ativo' }
-    ];
-
+    ativo: true },
+    {
+      nome: 'Sebastião da Silva', cidade: 'São Paulo' , estado: 'SP', 
+      ativo: false },
+    {
+      nome: 'Carla Souza', cidade: 'Florianópolis' , estado: 'SC', 
+      ativo: true },
+    {
+      nome: 'Luís Pereira', cidade: 'Curitiba' , estado: 'PR', 
+      ativo: true},
+    {
+      nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro' , estado: 'RJ', 
+      ativo: false 
+    },
+    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', 
+    ativo: true }  
+  ];
+  // pessoas = [];
 }
